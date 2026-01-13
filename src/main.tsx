dac52955dd990369f784649833e7238e4b06f5d6
Sergio Castro <sergiomvj@gmail.com>
2026-01-13 13:26:27 -0300

    chore: add version marker v2026.01.13.12.35 and console log for deployment verification

--- src/main.tsx
+++ src/main.tsx
@@ -4,4 +4,6 @@ import App from "./App.tsx";
 import "./index.css";
 import "./utils/i18n";
 
+console.log('App Version: 2026.01.13.12.35 - Cache Busting Active');
+
 createRoot(document.getElementById("root")!).render(<App />);
