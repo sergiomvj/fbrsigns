6e88208b9a228555723929d9326ec5a2d385bf9e
Sergio Castro <sergiomvj@gmail.com>
2026-01-13 13:30:30 -0300

    chore: bump version to v2026.01.13.13.00 to reflect auth fix

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,6 @@ import App from "./App.tsx";
 import "./index.css";
 import "./utils/i18n";
 
-console.log('App Version: 2026.01.13.12.35 - Cache Busting Active');
+console.log('App Version: 2026.01.13.13.00 - Auth Fix Applied');
 
 createRoot(document.getElementById("root")!).render(<App />);
